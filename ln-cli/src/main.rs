3d6843a9012ebb7a1b5e5779eadaaeedd6c8d9ad
HaxSam <haxsam@pm.me>
2022-04-02 23:17:48 +0200

    Changed the structer

--- ln-cli/src/main.rs
+++ ln-cli/src/main.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
 
-use ln_lib::lightnovel::{LightnovelCategory, LightnovelList};
+use ln_lib::{LightnovelCategory, LightnovelList};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-	let mut list = LightnovelList::new(LightnovelCategory::Genre(String::from("actions")))?;
+	let mut list = LightnovelList::new(LightnovelCategory::Genre(String::from("action")))?;
+	list.scrape().await?;
 
 	Ok(())
 }
