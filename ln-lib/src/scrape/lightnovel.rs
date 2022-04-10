c42ee46e73b8abe87bc896c3efe685e9d782fad1
HaxSam <haxsam@pm.me>
2022-04-10 12:35:27 +0200

    Resturcture lightnocel scrape
    Changed if to match for better readability also ownership of res_body goes to the function

--- ln-lib/src/scrape/lightnovel.rs
+++ ln-lib/src/scrape/lightnovel.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use scraper::{Html, Selector};
 use surf::Client;
 
-fn get_title_href(body: &String) -> Result<Vec<(String, String)>, Box<dyn Error>> {
+fn get_title_href(body: String) -> Result<Vec<(String, String)>, Box<dyn Error>> {
 	let document = Html::parse_document(&body);
 
 	let div_select = Selector::parse("div.home-truyendecu").unwrap();
@@ -25,59 +25,55 @@ fn get_title_href(body: &String) -> Result<Vec<(String, String)>, Box<dyn Error>
 pub async fn get_latest_ln(
 	client: &Client, page: i32,
 ) -> Result<Vec<(String, String)>, Box<dyn Error>> {
-	let path = if page == 1 {
-		"/latest".to_string()
-	} else {
-		format!("/latest/page/{}", page)
+	let path = match page {
+		1 => String::from("/latest"),
+		_ => format!("/latest/page/{}", page),
 	};
 
 	let mut res = client.get(path).await?;
 	let res_body = res.body_string().await?;
 
-	get_title_href(&res_body)
+	get_title_href(res_body)
 }
 
 pub async fn get_completed_ln(
 	client: &Client, page: i32,
 ) -> Result<Vec<(String, String)>, Box<dyn Error>> {
-	let path = if page == 1 {
-		"/completed".to_string()
-	} else {
-		format!("/completed/page/{}", page)
+	let path = match page {
+		1 => String::from("/completed"),
+		_ => format!("/completed/page/{}", page),
 	};
 
 	let mut res = client.get(path).await?;
 	let res_body = res.body_string().await?;
 
-	get_title_href(&res_body)
+	get_title_href(res_body)
 }
 
 pub async fn get_genre_ln(
 	client: &Client, genre: &String, page: i32,
 ) -> Result<Vec<(String, String)>, Box<dyn Error>> {
-	let path = if page == 1 {
-		format!("/{}", genre)
-	} else {
-		format!("/{}/page/{}", genre, page)
+	let path = match page {
+		1 => format!("/{}", genre),
+		_ => format!("/{}/page/{}", genre, page),
 	};
 
 	let mut res = client.get(path).await?;
 	let res_body = res.body_string().await?;
 
-	get_title_href(&res_body)
+	get_title_href(res_body)
 }
 
 pub async fn get_title_ln(
 	client: &Client, title: &String, page: i32,
 ) -> Result<Vec<(String, String)>, Box<dyn Error>> {
-	let path = if page == 1 {
-		format!("/?s={}", title)
-	} else {
-		format!("/page/{}?s={}", page, title)
+	let path = match page {
+		1 => format!("/?s={}", title),
+		_ => format!("/page/{}?s={}", page, title),
 	};
 
 	let mut res = client.get(path).await?;
 	let res_body = res.body_string().await?;
 
-	get_title_href(&res_body)
+	get_title_href(res_body)
 }
