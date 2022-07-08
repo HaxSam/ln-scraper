d9d11695ffb61a7cfbfd7ca3f65958817a94fa11
HaxSam <haxsam@pm.me>
2022-07-08 19:13:19 +0200

    show ln and chapters with skim

--- ln-cli/src/main.rs
+++ ln-cli/src/main.rs
@@ -1,16 +1,62 @@
+mod menu;
+
+use menu::{show_chapters, show_ln};
+
 use std::error::Error;
 
+extern crate skim;
+use clap::{AppSettings, ArgGroup, Parser};
 use ln_lib::{LightnovelCategory, LightnovelList};
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+#[clap(global_setting(AppSettings::DeriveDisplayOrder))]
+#[clap(group(ArgGroup::new("type").required(true).args(&["name", "genre", "completed", "latest"])))]
+struct Args {
+	/// search for the lightnovel you want to read
+	name: Option<String>,
+
+	/// get the lightnovels with the genre you want to read
+	#[clap(short)]
+	genre: Option<String>,
+
+	/// get all completed lightnovels
+	#[clap(short)]
+	completed: bool,
+
+	/// get the latest lightnovels
+	#[clap(short)]
+	latest: bool,
+
+	/// get staged lightnovel
+	#[clap(short)]
+	stage: bool,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-	let mut list = LightnovelList::new(LightnovelCategory::Latest)?;
-	list.scrape().await?;
-	let mut ln = list.get_lightnovel(1);
+	let args = Args::parse();
 
-	ln.scrape_chapter_page(None).await?;
-	let mut chapter = ln.get_lightnovel_chapter(1);
-	chapter.scrape_paragraph().await?;
+	let category = if let Some(name) = args.name {
+		LightnovelCategory::Title(name)
+	} else if let Some(genre) = args.genre {
+		LightnovelCategory::Genre(genre)
+	} else if args.completed {
+		LightnovelCategory::Completed
+	} else if args.latest {
+		LightnovelCategory::Latest
+	} else {
+		LightnovelCategory::Latest
+	};
+
+	let mut list = LightnovelList::new(category)?;
+	list.scrape().await?;
+
+	let mut ln = show_ln(&mut list).unwrap();
+
+	ln.scrape().await?;
+
+	let _chapter = show_chapters(&mut ln).await?;
 
 	Ok(())
 }
---
+++ ln-cli/src/menu.rs
@@ -0,0 +1,123 @@
+#![allow(unused_imports, dead_code)]
+
+use std::error::Error;
+use std::thread;
+
+extern crate skim;
+use ln_lib::{Lightnovel, LightnovelChapter, LightnovelList};
+use skim::prelude::*;
+
+struct LightnovelWrapper {
+	pub ln: Lightnovel,
+}
+
+struct LightnovelChapterWarpper {
+	pub chapter: LightnovelChapter,
+}
+
+impl SkimItem for LightnovelWrapper {
+	fn text(&self) -> Cow<str> {
+		Cow::Borrowed(self.ln.get_title())
+	}
+}
+
+impl SkimItem for LightnovelChapterWarpper {
+	fn text(&self) -> Cow<str> {
+		Cow::Borrowed(self.chapter.get_title())
+	}
+}
+
+pub fn show_ln(list: &mut LightnovelList) -> Option<Lightnovel> {
+	let (tx_item, rx_item): (SkimItemSender, SkimItemReceiver) = unbounded();
+
+	let options = SkimOptionsBuilder::default()
+		.height(Some("100%"))
+		.prompt(Some("Select a lightnovel> "))
+		.reverse(true)
+		.build()
+		.unwrap();
+
+	for ln in list {
+		let wrapper = LightnovelWrapper { ln: ln.clone() };
+		tx_item.send(Arc::new(wrapper)).unwrap();
+	}
+
+	drop(tx_item);
+
+	let selected_itemes = Skim::run_with(&options, Some(rx_item))
+		.map(|out| out.selected_items)
+		.unwrap_or_else(|| Vec::new());
+
+	let ln_wrapper_pointer = selected_itemes.into_iter().next();
+
+	match ln_wrapper_pointer {
+		Some(ln_wrapper_pointer) => {
+			let ln_wrapper: &LightnovelWrapper = (*ln_wrapper_pointer)
+				.as_any()
+				.downcast_ref::<LightnovelWrapper>()
+				.unwrap();
+			Some(ln_wrapper.ln.clone())
+		}
+		None => None,
+	}
+}
+
+pub async fn show_chapters(
+	ln: &mut Lightnovel,
+) -> Result<Option<LightnovelChapter>, Box<dyn Error>> {
+	let (tx_chapter, rx_chapter): (SkimItemSender, SkimItemReceiver) = unbounded();
+
+	let (tx, rx): (Sender<&str>, Receiver<&str>) = bounded(1);
+
+	let handle = thread::spawn(move || {
+		let options = SkimOptionsBuilder::default()
+			.height(Some("100%"))
+			.reverse(true)
+			.prompt(Some("Select chapter> "))
+			.multi(true)
+			.build()
+			.unwrap();
+
+		let ret = Skim::run_with(&options, Some(rx_chapter))
+			.map(|out| out.selected_items)
+			.unwrap_or_else(|| Vec::new());
+
+		tx.send("done").unwrap();
+
+		ret
+	});
+
+	loop {
+		for ch in ln.into_iter() {
+			let wrapper = LightnovelChapterWarpper {
+				chapter: ch.clone(),
+			};
+
+			if rx.try_recv().is_ok() {
+				break;
+			}
+
+			tx_chapter.send(Arc::new(wrapper)).unwrap();
+		}
+		let got_chapters = ln.scrape().await?;
+		if !got_chapters || rx.try_recv().is_ok() {
+			break;
+		}
+	}
+
+	drop(tx_chapter);
+
+	let selected_itemes = handle.join().unwrap();
+	let chapter_wrapper_pointer = selected_itemes.into_iter().next();
+
+	match chapter_wrapper_pointer {
+		Some(chapter_wrapper_pointer) => {
+			let chapter_wrapper: &LightnovelChapterWarpper = (*chapter_wrapper_pointer)
+				.as_any()
+				.downcast_ref::<LightnovelChapterWarpper>()
+				.unwrap();
+			Ok(Some(chapter_wrapper.chapter.clone()))
+		}
+		None => Ok(None),
+	}
+}
